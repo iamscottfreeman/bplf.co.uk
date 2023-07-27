23fcca181f829597923a356d51d6e34f319e5970
Scott Freeman <email@scottfreeman.net>
2023-07-27 11:11:52 +0100

    SendGrid email

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -1,63 +1,32 @@
-import nodemailer from 'nodemailer';
+const sgMail = require('@sendgrid/mail');
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const contactEmail = nodemailer.createTransport({
-      host: 'smtp.sendgrid.net',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'apikey',
-        pass: process.env.SENDGRID_API
-      }
-    });
+async function sendEmail(req, res) {
+  sgMail.setApiKey(process.env.SENDGRID_API);
 
-    await new Promise((resolve, reject) => {
-      contactEmail.verify((error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-
+  try {
     const email = req.body.email;
     const name = req.body.name;
+    const verifiedEmailAddress = 'email@scottfreeman.net';
+    const subject = 'BPF Website Contact Form Submission';
     const message = req.body.message.replace(/(?:\r\n|\r|\n)/g, '<br>');
 
-    const mailOptions = {
-      from: 'email@scottfreeman.net',
-      to: 'email@scottfreeman.net',
-      cc: email,
+    await sgMail.send({
+      to: verifiedEmailAddress,
+      from: verifiedEmailAddress,
       replyTo: `${name} <${email}>`,
-      subject: 'BPF Website Contact Form Submission',
+      subject,
+      text: req.body.message,
       html: `
-           <p><b>From:</b> ${name}</p>
-           <p><b>Email:</b> ${email}</p>
-           <p><b>Message:</b><br />${message}</p>
+          <p><b>From:</b> ${name}</p>
+          <p><b>Email:</b> ${email}</p>
+          <p><b>Message:</b><br />${message}</p>
       `
-    };
-
-    await new Promise((resolve, reject) => {
-      contactEmail.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(info);
-        }
-      });
     });
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-      JSON.stringify({
-        status: 'success'
-      })
-    );
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message });
   }
+
+  return res.status(200).json({ error: '' });
 }
+
+export default sendEmail;
