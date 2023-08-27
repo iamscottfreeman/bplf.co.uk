6345db174ede388bde4708814cfdc19630eff058
Scott Freeman <email@scottfreeman.net>
2023-08-27 10:27:04 +0100

    Updated British Records

--- src/pages/results/index.tsx
+++ src/pages/results/index.tsx
@@ -21,29 +21,35 @@ export default function Results() {
           <Container className='py-10'>
             <div className='md:grid grid-cols-3 items-center text-center gap-4'>
               <ResultLink
-                title='TESTED RAW SINGLE LIFT RECORDS AUGUST 2023'
-                link='/downloads/TESTED-RAW-SINGAL-LIFT-records.xlsx'
+                title='TESTED FULL POWER CLASSIC RAW AUGUST 2023'
+                link='/downloads/TESTED-FP-CLASSIC-RAW.xlsx'
+              />
+              <ResultLink title='TESTED FULL POWER RAW AUGUST 2023' link='/downloads/TESTED-FP-RAW.xlsx' />
+              <ResultLink
+                title='TESTED CLASSIC CLASSIC RAW SINGLE LIFTS AUGUST 2023'
+                link='/downloads/TESTED-CLASSIC-RAW-SINGLE-LIFTS.xlsx'
               />
               <ResultLink
-                title='TESTED FP CLASSIC RAW RECORDS AUGUST 2023'
-                link='/downloads/Tested-FP-RAW-records-template.xslx'
+                title='TESTED CLASSIC POWER RAW SINGLE LIFTS AUGUST 2023'
+                link='/downloads/TESTED-RAW-SINGLE-LIFTS.xlsx'
+              />
+              <ResultLink title='TESTED STRICT CURL AUGUST 2023' link='/downloads/TESTED-STRICT-CURL.xlsx' />
+
+              <ResultLink
+                title='NON-TESTED FULL POWER CLASSIC RAW AUGUST 2023'
+                link='/downloads/NON-TESTED-FP-CLASSIC-RAW.xlsx'
+              />
+              <ResultLink title='NON-TESTED FULL POWER RAW AUGUST 2023' link='/downloads/NON-TESTED-FP-RAW.xlsx' />
+              <ResultLink
+                title='TESTED CLASSIC CLASSIC RAW SINGLE LIFTS AUGUST 2023'
+                link='/downloads/NON-TESTED-CLASSIC-RAW-SINGLE-LIFTS.xlsx'
               />
               <ResultLink
-                title='NON TESTED RAW SINGLE LIFT RECORDS MAY 2023'
-                link='/downloads/NON-TESTED-RAW-SINGAL-LIFT-records-28-MAY-2023-1.xlsx'
+                title='NON-TESTED CLASSIC POWER RAW SINGLE LIFTS AUGUST 2023'
+                link='/downloads/NON-TESTED-RAW-SINGLE-LIFTS.xlsx'
               />
-              <ResultLink
-                title='NON TESTED FP CLASSIC RAW RECORDS MAY 2023'
-                link='/downloads/NON-Tested-FP-CLASSIC-RAW-records.xlsx'
-              />
-              <ResultLink
-                title='TESTED STRICT CURL APRIL 2023'
-                link='/downloads/TESTED-STRICKED-CURL-23-APRIL-2023.xlsx'
-              />
-              <ResultLink
-                title='NON TESTED STRICT CURL APRIL 2023'
-                link='/downloads/NON-TESTED-STRICKED-CURL-23-APRIL-2023.xlsx'
-              />
+              <ResultLink title='NON-TESTED STRICT CURL AUGUST 2023' link='/downloads/NON-TESTED-STRICT-CURL.xlsx' />
+
               <ResultLink title='Mens Para Bench May 2023' link='/downloads/mens-para-bench-2023.xlsx' />
               <ResultLink title='Mens Para Deadlift May 2023' link='/downloads/mens-para-deadlift-2023.xlsx' isLast />
             </div>
