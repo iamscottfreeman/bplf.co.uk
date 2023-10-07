3625a6c705f560eb3156a2452a7ea75ebf85f006
Scott Freeman <email@scottfreeman.net>
2023-10-07 13:25:23 +0100

    Events page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -9,14 +9,11 @@ import PageHeader from '@/components/atoms/PageHeader';
 import Container from '@/components/atoms/Container';
 import Button from '@/components/atoms/Button';
 
-import img03 from './03.jpg';
-import img04 from './04.jpg';
-import img05 from './05.jpg';
 import img06 from './06.jpg';
 import img07 from './07.jpg';
-import img08 from './08.jpg';
-import img09 from './09.jpg';
 import img10 from './10.jpg';
+import recordbreakers from './recordbreakers.jpg';
+import montycrown from './montycrown.jpg';
 
 export default function Events() {
   return (
@@ -31,69 +28,6 @@ export default function Events() {
           <PageHeader title='Events' />
           <Container className='py-10'>
             <div className='lg:grid grid-cols-2 space-x-4 space-y-10 -ml-4 -mt-10'>
-              <div className='ml-4 mt-10 flex flex-col items-center text-center'>
-                <Image src={img04} alt='' className='w-3/4 rounded-md mb-4' />
-                <h2 className={`${fonts.header.className} uppercase text-red-600 text-4xl mb-2`}>Women of Iron</h2>
-                <p className={`${fonts.header.className} text-3xl uppercase mb-2`}>27th August 2023</p>
-                <p className='mb-2'>Prize meet</p>
-                <p className='mb-2'>
-                  The BPF Academy
-                  <br />
-                  Unit 3 Baily Brook Business Park
-                  <br />
-                  Langley Mill
-                  <br />
-                  NG16 4BE
-                </p>
-                <p className='mb-2'>
-                  <Button href='https://forms.gle/MjNyoChmXHYdkQ1KA' target='_blank'>
-                    Entry Form
-                  </Button>
-                </p>
-              </div>
-              <div className='ml-4 mt-10 flex flex-col items-center text-center'>
-                <Image src={img03} alt='' className='w-3/4 rounded-md mb-4' />
-                <h2 className={`${fonts.header.className} uppercase text-red-600 text-4xl mb-2`}>Mayhem Summer Bash</h2>
-                <p className={`${fonts.header.className} text-3xl uppercase mb-2`}>17th September 2023</p>
-                <p className='mb-2'>Qualifier for 2024</p>
-                <p className='mb-2'>
-                  Mayhem Gym
-                  <br />
-                  6 Bittern Way
-                  <br />
-                  Boston
-                  <br />
-                  PE21 7NX
-                </p>
-                <p className='mb-2'>
-                  <Button href='https://forms.gle/mKtLC9rL6t4PfjZe8' target='_blank'>
-                    Entry Form
-                  </Button>
-                </p>
-              </div>
-              <div className='ml-4 mt-10 flex flex-col items-center text-center'>
-                <Image src={img05} alt='' className='w-3/4 rounded-md mb-4' />
-                <h2 className={`${fonts.header.className} uppercase text-red-600 text-4xl mb-2`}>Day of the Dead</h2>
-                <p className={`${fonts.header.className} text-3xl uppercase mb-2`}>1st October 2023</p>
-                <p className='mb-2'>Qualifier for 2024</p>
-                <p className='mb-2'>
-                  The BPF Academy
-                  <br />
-                  Unit 3 Baily Brook Business Park
-                  <br />
-                  Langley Mill
-                  <br />
-                  NG16 4BE
-                </p>
-                <p className='mb-2'>
-                  <Button
-                    href='https://docs.google.com/forms/d/e/1FAIpQLSeq68Hem-jVzJ5mBVu5j6BGEhC8fO1vDYg85QRcusoVIzZl_A/viewform?vc=0&c=0&w=1&flr=0'
-                    target='_blank'
-                  >
-                    Entry Form
-                  </Button>
-                </p>
-              </div>
               <div className='ml-4 mt-10 flex flex-col items-center text-center'>
                 <Image src={img06} alt='' className='w-3/4 rounded-md mb-4' />
                 <h2 className={`${fonts.header.className} uppercase text-red-600 text-4xl mb-2`}>
@@ -125,7 +59,7 @@ export default function Events() {
                 <p className='mb-2'>By invite only</p>
               </div>
               <div className='ml-4 mt-10 flex flex-col items-center text-center'>
-                <Image src={img08} alt='' className='w-3/4 rounded-md mb-4' />
+                <Image src={recordbreakers} alt='' className='w-3/4 rounded-md mb-4' />
                 <h2 className={`${fonts.header.className} uppercase text-red-600 text-4xl mb-2`}>Record Breakers</h2>
                 <p className={`${fonts.header.className} text-3xl uppercase mb-2`}>26th November 2023</p>
                 <p className='mb-2'>Qualifier for 2024</p>
@@ -139,13 +73,20 @@ export default function Events() {
                   NG16 4BE
                 </p>
                 <p className='mb-2'>
+                  Weigh in: 25th November 2023
+                  <br />
+                  10:00 - 12:00 - No appointment required
+                  <br />
+                  17:00 - 18:00 - By appointment only
+                </p>
+                <p className='mb-2'>
                   <Button href='https://forms.gle/s2AEEvyQihuCYtqAA' target='_blank'>
                     Entry Form
                   </Button>
                 </p>
               </div>
               <div className='ml-4 mt-10 flex flex-col items-center text-center'>
-                <Image src={img09} alt='' className='w-3/4 rounded-md mb-4' />
+                <Image src={montycrown} alt='' className='w-3/4 rounded-md mb-4' />
                 <h2 className={`${fonts.header.className} uppercase text-red-600 text-4xl mb-2`}>The Monty Crown</h2>
                 <p className={`${fonts.header.className} text-3xl uppercase mb-2`}>3rd - 4th February 2024</p>
                 <p className='mb-2'>
@@ -157,6 +98,7 @@ export default function Events() {
                   <br />
                   NG16 4BE
                 </p>
+                <p className='mb-2'>Weigh in: 2nd February 2024</p>
                 <p className='mb-2'>
                   <Button href='https://forms.gle/6hzVuQntesn4nchS6' target='_blank'>
                     Entry Form
