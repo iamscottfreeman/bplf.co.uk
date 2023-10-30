74da066fe9426b46ad4b92c85f958902a5d12521
Scott Freeman <email@scottfreeman.net>
2023-10-30 08:58:52 +0000

    Comp results

--- src/pages/results/competitions.tsx
+++ src/pages/results/competitions.tsx
@@ -22,19 +22,23 @@ export default function CompetitionResults() {
           <Container className='py-10'>
             <h2 className={`${fonts.header.className} uppercase text-center text-5xl mb-4 text-red-600`}>2023</h2>
             <div className='md:grid grid-cols-3 items-center text-center gap-4'>
+              <ResultLink
+                title='Drug Tested World Championships 18th till 22nd October 2023'
+                link='/downloads/DrugTestedWorldChampionships2023.xlsx'
+              />
               <ResultLink title='Mayhem-in-Boston - 25th March 2023' link='/downloads/Mayhem-in-Boston-112233.xlsx' />
-              {/* <ResultLink
-                title='IPL Commonwealth and European Championships 2023 - Day 1'
+              <ResultLink
+                title='Commonwealth and European Championships 2023 - Day 1'
                 link='/downloads/E&C-Open-2023-Day1.xlsx'
               />
               <ResultLink
-                title='IPL Commonwealth and European Championships 2023 - Day 2'
+                title='Commonwealth and European Championships 2023 - Day 2'
                 link='/downloads/E&C-Open-2023-Day2.xlsx'
               />
               <ResultLink
-                title='IPL Commonwealth and European Championships 2023 - Day 3'
+                title='Commonwealth and European Championships 2023 - Day 3'
                 link='/downloads/E&C-Open-2023-Day3.xlsx'
-              /> */}
+              />
               <ResultLink title='BOSTON 2023' link='/downloads/BOSTON-17092023.xlsx' />
               <ResultLink title='WOMEN OF IRON 2023' link='/downloads/WOMEN-OF-IRON-2023.xlsx' />
               <ResultLink title='DAY OF THE DEAD 2023' link='/downloads/DAY-OF-THE-DEAD-FULL-COM.xls' />
