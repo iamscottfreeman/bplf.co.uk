a9177bf1b7b9d6317001561268d4c05019a838f9
Scott Freeman <email@scottfreeman.net>
2023-11-20 09:41:03 +0000

    Strict curl records

--- src/pages/strict-curl/records.tsx
+++ src/pages/strict-curl/records.tsx
@@ -6,6 +6,7 @@ import Footer from '@/components/footer/Footer';
 import PageHeader from '@/components/atoms/PageHeader';
 import Container from '@/components/atoms/Container';
 import ResultLink from '@/components/atoms/ResultLink';
+import { fonts } from '@/helpers/fonts';
 
 export default function Records() {
   return (
@@ -25,7 +26,6 @@ export default function Records() {
                 title='WORLD NON-TESTED STRICT CURL NOVEMBER 2023'
                 link='/downloads/NON-TESTED-STRICT-CURL.xlsx'
               />
-              <ResultLink title='MAYHEM IN BOSTON 2023' link='/downloads/Mayhem-in-Boston-strict-curl-17092023.csv' />
               <ResultLink
                 title='TESTED STRICT CURL APRIL 2023'
                 link='/downloads/TESTED-STRICKED-CURL-23-APRIL-2023.xlsx'
@@ -33,6 +33,16 @@ export default function Records() {
               <ResultLink
                 title='NON TESTED STRICT CURL APRIL 2023'
                 link='/downloads/NON-TESTED-STRICKED-CURL-23-APRIL-2023.xslx'
+                isLast
+              />
+            </div>
+            <h2 className={`${fonts.header.className} uppercase text-5xl mt-20 mb-8 text-center`}>
+              Previous Competition Results
+            </h2>
+            <div className='md:grid grid-cols-3 items-center text-center gap-4'>
+              <ResultLink
+                title='MAYHEM IN BOSTON 2023'
+                link='/downloads/Mayhem-in-Boston-strict-curl-17092023.csv'
                 isLast
               />
             </div>
