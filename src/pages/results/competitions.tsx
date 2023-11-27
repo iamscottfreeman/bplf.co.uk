f60accb3b94525fad27e2dad8d46bd7f026ba048
Scott Freeman <email@scottfreeman.net>
2023-11-27 09:46:42 +0000

    Comp

--- src/pages/results/competitions.tsx
+++ src/pages/results/competitions.tsx
@@ -22,6 +22,7 @@ export default function CompetitionResults() {
           <Container className='py-10'>
             <h2 className={`${fonts.header.className} uppercase text-center text-5xl mb-4 text-red-600`}>2023</h2>
             <div className='md:grid grid-cols-3 items-center text-center gap-4'>
+              <ResultLink title='RECORD-BRAKERS-2023' link='/downloads/RECORD-BRAKERS-2023.xslx' />
               <ResultLink
                 title='100% RAW STRICT CURL WORLD CUP 2023 GB'
                 link='/downloads/100-RAW-STRICT-CURL-WORLD-CUP-2023-GB.xlsx'
